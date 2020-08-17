574cd518938c57fbdaf5d6bfe3f2fe896ab36ded
رزا <36600639+reza-tanha@users.noreply.github.com>
2020-08-17 13:17:06 +0430

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,11 +2,11 @@
 
 echo -e "\e[93\t\tmWellcom to GST-Lfi Tools"
 pwwd=$(pwd)
+chmod +x GST-Lfi.py
 dis=${pwwd}"/GST-Lfi.py"
 ln -s $dis /usr/bin/gst-lfi
 gst="/usr/bin/gst-lfi"
 if [ -L $gst ];then
-	sudo rm -r /usr/bin/gst-lfi
 	echo -e "\e[32mConfig Successful ✅\n"
 	echo -e "\e[92mExample :\n\t \e[95mgst-lfi -h"
 	echo -e "\e[95m\t gst-lfi -u http://site.com/index.php?page="
