afc8c5851f60082bbcc4787e6673d9416474d3d6
رزا <36600639+reza-tanha@users.noreply.github.com>
2020-08-17 12:23:12 +0430

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\e[93\t\tmWellcom to GST-Lfi Tools"
+pwwd=$(pwd)
+dis=${pwwd}"/gst-lfi.py"
+ln -s $dis /usr/bin/gst-lfi
+gst="/usr/bin/gst-lfi"
+if [ -L $gst ];then
+	sudo rm -r /usr/bin/gst-lfi
+	echo -e "\e[32mConfig Successful ✅\n"
+	echo -e "\e[92mExample :\n\t \e[95mgst-lfi -h"
+	echo -e "\e[95m\t gst-lfi -u http://site.com/index.php?page="
+	echo -e "\e[95m\t gst-lfi -u http://site.com/index.php?page= -p 127.0.0.1:9050"
+else
+	echo -e "\e[5m\e[31mErorr ! :(\e[25m"
+	echo -e "\e[32mpython3 GST-Lfi.py -h"
+fi
